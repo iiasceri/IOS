e62ef5407b7fa6a7254021e4efc34fae248a0141
iiasceri <iiasceri@student.42.fr>
2018-08-20 18:12:39 +0300

    Added several apps

---
+++ App Development/MVC-Quizz/Quizzler/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Angela Yu on 25/08/2015.
+//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let allQuestions = QuestionBank()
+    
+    
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var progressBar: UIView!
+    @IBOutlet weak var progressLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        let firstQuestion = allQuestions.list[0]
+        questionLabel.text = firstQuestion.questionText
+    }
+
+
+    @IBAction func answerPressed(_ sender: AnyObject) {
+  
+    }
+    
+    
+    func updateUI() {
+      
+    }
+    
+
+    func nextQuestion() {
+        
+    }
+    
+    
+    func checkAnswer() {
+        
+    }
+    
+    
+    func startOver() {
+       
+    }
+    
+
+    
+}
